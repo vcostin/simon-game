37a492812efdc2f21bea4097b4f863743637f13a
Vadim Costin <vadim.costin@yopeso.com>
2017-05-11 11:18:00 +0300

    Refactore using setInterval

--- src/components/SimonDevice.js
+++ src/components/SimonDevice.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import SimonButton from './SimonButton';
 
-
-// gen a number between 1-4
-//
-
 const sounds = [
   { id: 1, src: 'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3', color: 'green' },
   { id: 2, src: 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3', color: 'red' },
@@ -15,8 +11,9 @@ const sounds = [
   return ({ id, color, audio: new Audio(src) });
 });
 
+// gen a number between 1-4
+//
 const simonRand = () => Math.floor(Math.random() * sounds.length) + 1;
-const timeouts = [];
 
 
 class SimonDevice extends Component {
@@ -24,33 +21,44 @@ class SimonDevice extends Component {
     super(props);
     this.state = {
       simonOrder: [1, 2, 3, 4, 2, 1, 3],
+      simonOrderIndex: 0,
+      timer: 0,
+      isPlaying: false,
     };
     this.handleButtonClickAction = this.handleButtonClickAction.bind(this);
     this.playSimonSequence = this.playSimonSequence.bind(this);
-    this.payTimedSound = this.payTimedSound.bind(this);
+    this.stopPlaySequence = this.stopPlaySequence.bind(this);
+    this.playNext = this.playNext.bind(this);
   }
 
   handleButtonClickAction(soundId) {
     console.log(soundId);
   }
 
-  payTimedSound(index, timeout) {
-    timeouts.push(setTimeout(() => {
-      const findSound = sounds.find(sound => (sound.id === this.state.simonOrder[index]));
-      findSound.audio.play();
-      console.log('play %d sequence', this.state.simonOrder[index]);
-    }, timeout));
+  playSimonSequence() {
+    this.stopPlaySequence();
+    this.setState({
+      timer: setInterval(this.playNext, 1000),
+    });
   }
 
-  playSimonSequence() {
-    for (let i = 0; i < this.state.simonOrder.length; i += 1) {
-      this.payTimedSound(i, 1000 * i);
+  playNext() {
+    const findSound = sounds.find(sound => (sound.id === this.state.simonOrder[this.state.simonOrderIndex]));
+    console.log('play %d sequence', findSound.id);
+    findSound.audio.play();
+    this.setState({
+      simonOrderIndex: this.state.simonOrderIndex + 1,
+    });
+    if (this.state.simonOrderIndex === this.state.simonOrder.length) {
+      this.stopPlaySequence();
     }
   }
 
   stopPlaySequence() {
-    timeouts.forEach((item) => {
-      clearTimeout(item);
+    clearInterval(this.state.timer);
+    this.setState({
+      timer: 0,
+      simonOrderIndex: 0,
     });
   }
 
@@ -65,7 +73,7 @@ class SimonDevice extends Component {
             onButtonClickAction={this.handleButtonClickAction}
             key={sound.id}
             soundId={sound.id}
-            audio={sound.audio} s
+            audio={sound.audio}
             backgroud={sound.color}
           />
         ))}
